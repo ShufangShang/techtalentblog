3fcd7924c3966b7e12c92b94f0eb664df8162c50
Shufang Shang <shufang.x.shang@gmail.com>
2023-04-18 19:56:02 -0500

    Edit post and delete post work now

--- src/main/java/com/techtalentsouth/techtalentblog/BlogPost/BlogPostController.java
+++ src/main/java/com/techtalentsouth/techtalentblog/BlogPost/BlogPostController.java
@@ -2,7 +2,8 @@ package com.techtalentsouth.techtalentblog.BlogPost;
 
 import java.util.ArrayList;
 import java.util.List;
- 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -68,5 +69,38 @@ public class BlogPostController {
         */
         return "blogpost/index";
     
+    }   
+   
+
+    @RequestMapping(value = "/blogposts/{id}", method = RequestMethod.GET)
+    public String editPostWithId(@PathVariable Long id, BlogPost blogPost, Model model) {
+        Optional<BlogPost> post = blogPostRepository.findById(id);
+        if (post.isPresent()) {
+            BlogPost actualPost = post.get();
+            model.addAttribute("blogPost", actualPost);
+        }
+        return "blogpost/edit";
     }
+   
+    @RequestMapping(value = "/blogposts/update/{id}")
+    public String updateExistingPost(@PathVariable Long id, BlogPost blogPost, Model model) {
+        Optional<BlogPost> post = blogPostRepository.findById(id);
+        if (post.isPresent()) {
+            BlogPost actualPost = post.get();
+            actualPost.setTitle(blogPost.getTitle());
+            actualPost.setAuthor(blogPost.getAuthor());
+            actualPost.setBlogEntry(blogPost.getBlogEntry());
+            blogPostRepository.save(actualPost);
+            model.addAttribute("blogPost", actualPost);
+        }
+
+        return "blogpost/result";
+    }
+
+    @RequestMapping(value = "blogposts/delete/{id}")
+    public String deletePostById(@PathVariable Long id, BlogPost blogPost) {
+        blogPostRepository.deleteById(id);
+        return "blogpost/delete";
+    }
+
 }
